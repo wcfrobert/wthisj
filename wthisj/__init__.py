5d0a032958132adc851fa343f0227b5baa1bc6fa
Robert W <55780110+wcfrobert@users.noreply.github.com>
2025-05-01 23:01:31 -0700

    Update __init__.py

--- wthisj/__init__.py
+++ wthisj/__init__.py
@@ -2,7 +2,7 @@
 wthisj is a python package that can perform punching shear stress calculations.
 """
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __author__ = "Robert Wang"
 __license__ = "MIT"
 
